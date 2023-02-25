307917577e9a6fa089d6ffb0eea4d2e6cf2e9c25
Mohammed Isma <mohammedismais@hotmail.com>
2023-02-25 09:23:02 +0100

    Add INVALID

--- database/seeders/PoliticalPartiesSeeder.php
+++ database/seeders/PoliticalPartiesSeeder.php
@@ -16,24 +16,25 @@ class PoliticalPartiesSeeder extends Seeder
     {
         PoliticalParty::truncate();
         PoliticalParty::insert([
-            [ "logo" => "/images/parties/A.jpg", "name" => "Accord", "slug"=>"A"],
-            [ "logo" => "/images/parties/AA.jpg", "name" => "Action Alliance", "slug"=>"AA"],
-            [ "logo" => "/images/parties/ADP.png", "name"=>"Action Democratic Party", "slug"=>"ADP"],
-            [ "logo" => "/images/parties/APP.png", "name"=>"Action Peoples Party", "slug"=>"APP"],
-            [ "logo" => "/images/parties/AAC.jpg", "name" => "African Action Congress", "slug"=>"AAC"],
-            [ "logo" => "/images/parties/ADC.jpg", "name" => "African Democratic Congress", "slug"=>"ADC"],
-            [ "logo" => "/images/parties/apclogo.jpg", "name" => "All Progressives Congress", "slug"=>"APC"],
-            [ "logo" => "/images/parties/APGA.jpg", "name" => "All Progressives Grand Alliance", "slug"=>"APGA"],
-            [ "logo" => "/images/parties/APM.jpg", "name" => "Allied Peoples Movement", "slug"=>"APM"],
-            [ "logo" => "/images/parties/BootParty.jpg", "name" => "Boot Party", "slug"=>"BP"],
-            [ "logo" => "/images/parties/LP.jpg", "name" => "Labour Party", "slug"=>"LP"],
-            [ "logo" => "/images/parties/NRM.png", "name"=>"National Rescue Movement", "slug"=>"NRM"],
-            [ "logo" => "/images/parties/NNPP.jpg", "name" => "New Nigeria Peoples Party", "slug"=>"NNPP"],
-            [ "logo" => "/images/parties/PDP.jpg", "name" => "Peoples Democratic Party", "slug"=>"PDP"],
-            [ "logo" => "/images/parties/PRP.png", "name"=>"Peoples Redemption Party", "slug"=>"PRP"],
-            [ "logo" => "/images/parties/SDP.png", "name"=>"Social Democratic Party", "slug"=>"SDP"],
-            [ "logo" => "https://inecnigeria.org/wp-content/uploads/2021/04/YPP-Logo.jpeg", "name"=>"Young Progressive Party", "slug"=>"YPP"],
-            [ "logo" => "/images/parties/ZLP.jpg", "name" => "Zenith Labour Party", "slug"=>"ZLP"]
+            [ "logo" => "/image/parties/A.jpg", "name" => "Accord", "slug"=>"A"],
+            [ "logo" => "/image/parties/AA.jpg", "name" => "Action Alliance", "slug"=>"AA"],
+            [ "logo" => "/image/parties/ADP.png", "name"=>"Action Democratic Party", "slug"=>"ADP"],
+            [ "logo" => "/image/parties/APP.png", "name"=>"Action Peoples Party", "slug"=>"APP"],
+            [ "logo" => "/image/parties/AAC.jpg", "name" => "African Action Congress", "slug"=>"AAC"],
+            [ "logo" => "/image/parties/ADC.jpg", "name" => "African Democratic Congress", "slug"=>"ADC"],
+            [ "logo" => "/image/parties/apclogo.jpg", "name" => "All Progressives Congress", "slug"=>"APC"],
+            [ "logo" => "/image/parties/APGA.jpg", "name" => "All Progressives Grand Alliance", "slug"=>"APGA"],
+            [ "logo" => "/image/parties/APM.jpg", "name" => "Allied Peoples Movement", "slug"=>"APM"],
+            [ "logo" => "/image/parties/BootParty.jpg", "name" => "Boot Party", "slug"=>"BP"],
+            [ "logo" => "/image/parties/LP.jpg", "name" => "Labour Party", "slug"=>"LP"],
+            [ "logo" => "/image/parties/NRM.png", "name"=>"National Rescue Movement", "slug"=>"NRM"],
+            [ "logo" => "/image/parties/NNPP.jpg", "name" => "New Nigeria Peoples Party", "slug"=>"NNPP"],
+            [ "logo" => "/image/parties/PDP.jpg", "name" => "Peoples Democratic Party", "slug"=>"PDP"],
+            [ "logo" => "/image/parties/PRP.png", "name"=>"Peoples Redemption Party", "slug"=>"PRP"],
+            [ "logo" => "/image/parties/SDP.png", "name"=>"Social Democratic Party", "slug"=>"SDP"],
+            [ "logo" => "/image/parties/YPP-Logo.jpeg", "name"=>"Young Progressive Party", "slug"=>"YPP"],
+            [ "logo" => "/image/parties/ZLP.jpg", "name" => "Zenith Labour Party", "slug"=>"ZLP"],
+            [ "logo" => "/image/parties/invalid.png", "name" => "**INVALID**", "slug"=>"invalid"]
         ]);
     }
 }
